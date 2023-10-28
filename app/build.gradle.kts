deef2f799b8f5ce1b8b0cf3b4179549f764d3d5c
Aiden Chang <asdf62210@gmail.com>
2023-10-28 12:58:48 +0800

    Use slf4j-android:1.7.30 instead
    Avoid Failed to load class org.slf4j.impl.StaticLoggerBinder
    https://www.slf4j.org/codes.html#StaticLoggerBinder

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
@@ -73,4 +73,7 @@ dependencies {
     implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.2")
     implementation("com.google.code.gson:gson:2.8.9")
     implementation("io.coil-kt:coil-compose:2.4.0")
+
+    // slf4j port for Android
+    implementation("org.slf4j:slf4j-android:1.7.30")
 }
