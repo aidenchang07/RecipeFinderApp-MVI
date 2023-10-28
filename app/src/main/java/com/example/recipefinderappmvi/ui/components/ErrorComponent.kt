94223e5ca01e16e9f7f12b2ceac1626e651c783e
Aiden Chang <asdf62210@gmail.com>
2023-10-28 13:13:29 +0800

    Zhongshan Home - v5 Complete

--- app/src/main/java/com/example/recipefinderappmvi/ui/components/ErrorComponent.kt
+++ app/src/main/java/com/example/recipefinderappmvi/ui/components/ErrorComponent.kt
@@ -1,6 +1,13 @@
 package com.example.recipefinderappmvi.ui.components
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 /**
  * Created by AidenChang 2023/10/27
@@ -8,5 +15,14 @@ import androidx.compose.runtime.Composable
 
 @Composable
 fun ErrorComponent(message: String, onRefreshClicked: () -> Unit) {
-
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = message)
+        Button(onClick = onRefreshClicked) {
+            Text(text = "Refresh")
+        }
+    }
 }
--- app/src/main/java/com/example/recipefinderappmvi/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/recipefinderappmvi/ui/screens/HomeScreen.kt
@@ -1,7 +1,9 @@
 package com.example.recipefinderappmvi.ui.screens
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import com.example.recipefinderappmvi.ui.components.ErrorComponent
 import com.example.recipefinderappmvi.ui.components.LoadingComponent
 import com.example.recipefinderappmvi.ui.components.SuccessComponent
 import com.example.recipefinderappmvi.ui.viewmodel.RecipeViewIntent
@@ -20,15 +22,19 @@ fun HomeScreen(recipeViewModel: RecipeViewModel) {
         RecipeViewState.Loading -> LoadingComponent()
         is RecipeViewState.Success -> {
             val recipes = (state as RecipeViewState.Success).recipes
-            SuccessComponent(recipes = recipes) {
-                recipeViewModel.processIntent(RecipeViewIntent.SearchRecipe(it))
-            }
+            SuccessComponent(recipes = recipes, onSearchClicked = { query ->
+                recipeViewModel.processIntent(RecipeViewIntent.SearchRecipe(query))
+            })
         }
         is RecipeViewState.Error -> {
             val message = (state as RecipeViewState.Error).message
-            ErrorComponent(message = message) {
+            ErrorComponent(message = message, onRefreshClicked = {
                 recipeViewModel.processIntent(RecipeViewIntent.LoadRandomRecipe)
-            }
+            })
         }
+    }
+
+    LaunchedEffect(Unit) {
+        recipeViewModel.processIntent(RecipeViewIntent.LoadRandomRecipe)
     }
 }
